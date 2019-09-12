9452e9d4e88f16afa9e022c025d38b933fcd316b
Vasilijs SPECTRE <v1ac95@gmail.com>
2019-09-12 14:22:14 +0300

    Created many stuff and made 2 new classes

---
+++ app/src/main/java/com/millenial/smellslikebakin/IngredientsFragment.java
@@ -0,0 +1,19 @@
+package com.millenial.smellslikebakin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class IngredientsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ingredients, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/millenial/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/millenial/smellslikebakin/ViewPagerFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class ViewPagerFragment extends Fragment {
 
@@ -15,7 +18,28 @@ public class ViewPagerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
+        getActivity().setTitle(Recipes.names[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
